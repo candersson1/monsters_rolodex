fc3b8c42adc22be090d91ab0e247d16601f5305b
Chris Andersson <candersson@klarsolutions.com>
2019-08-25 14:27:54 -0400

    Updates

---
+++ precache-manifest.1e5e59c4f02b952f261204dd6b8dfae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7c23ce057d67e12da7f34f63b091f1",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "d5e61f1c9982b5554c61",
+    "url": "/monsters_rolodex/static/css/main.67ddd14b.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters_rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "d5e61f1c9982b5554c61",
+    "url": "/monsters_rolodex/static/js/main.8713fd0d.chunk.js"
+  },
+  {
+    "revision": "c840d8816b0ae4f05802",
+    "url": "/monsters_rolodex/static/js/runtime~main.6f3489d2.js"
+  }
+]);
